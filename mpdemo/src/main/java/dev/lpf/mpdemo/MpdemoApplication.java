244b3382abbdec4ae1384fa49f773cce7bf19830
RunAtWorld <hudalpf@163.com>
2020-05-28 11:00:09 +0800

    update mpdemo/src/main/java/dev/lpf/mpdemo/MpdemoApplication.java.

--- mpdemo/src/main/java/dev/lpf/mpdemo/MpdemoApplication.java
+++ mpdemo/src/main/java/dev/lpf/mpdemo/MpdemoApplication.java
@@ -1,15 +1,24 @@
-package dev.lpf.mpdemo;
+
+package dev;
+
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@MapperScan("dev.lpf.mpdemo.*.mapper")
-@SpringBootApplication
-public class MpdemoApplication {
+/*
+DruidDataSourceAutoConfigure会注入一个DataSourceWrapper，
+        其会在原生的spring.datasource下找url,username,password等。
+        而我们动态数据源的配置路径是变化的,所以需要排除*/
+@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
+@MapperScan({"dev.lpf.*.mapper"})
+@ComponentScan({"dev.lpf"})
+public class MPDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MpdemoApplication.class, args);
+        SpringApplication.run(MPDemoApplication.class, args);
     }
 
 }
